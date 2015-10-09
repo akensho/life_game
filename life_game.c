4671d9a7032ed5cd9230e88c0e9a16972100da64
Kensho Aoki <kensho.aoki@gmail.com>
2015-10-10 03:30:24 +0900

    2次元配列っぽく扱えるようにして、変数名をfieldからmatrixに変更した

--- life_game.c
+++ life_game.c
@@ -2,29 +2,44 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-bool *initField(int n, int m){
-  bool *field;
-  field = malloc(sizeof(bool) * n * m);
+char **initMatrix(int n, int m){
+  char **matrix;
+  matrix = malloc(sizeof(char *) * n);
+  for(int i = 0; i < n; i++){
+    matrix[i] = malloc(sizeof(char) * m);
+  }
   for(int i = 0; i < n; i++){
     for(int j = 0; j < m; j++){
-      field[i * m + j] = false;
+      matrix[i][j] = '?';
     }
   }
-  return field;
+  return matrix;
 }
 
-void prinfField(){
-
+void prinfMatrix(char **matrix, int n, int m){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      printf("%c ", matrix[i][j]);
+    }
+    printf("\n");
+  }
 }
 
+void freeMatrix(char **matrix, int n){
+  for(int i = 0; i < n; i++){
+    free(matrix[i]);
+  }
+}
 
 int main(int argc, char *argv[]){
   if( argc != 3 ){
-    printf("Error: => 'ex_command' 'row' 'column'/n");
+    printf("Error: => 'ex_command' 'row' 'column'\n");
     exit(1);
   }
   const int N = atoi(argv[1]);
   const int M = atoi(argv[2]);
-  bool *field = initField(N, M);
+  char **matrix = initMatrix(N, M);
+  prinfMatrix(matrix, N, M);
+  freeMatrix(matrix, N);
   return 0;
 }
